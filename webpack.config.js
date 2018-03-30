0a57a8b10eb7e48f3ad7f2fa8c527c31a6c13add
jphamilton <instantjp@gmail.com>
2018-03-30 13:27:58 -0500

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
 var webpack = require('webpack');
 
 module.exports = {  
+  mode: 'production',
   entry: './src/loader.ts',
+  optimization: {
+    minimize: true
+  },
   output: {
     filename: './build/loader.js'
   },
   resolve: {
     extensions: ['.webpack.js', '.web.js', '.ts', '.js']
   },
-  // plugins: [
-  //   new webpack.optimize.UglifyJsPlugin()
-  // ],
   module: {
-    loaders: [
+    rules: [
       { test: /\.ts$/, loader: 'ts-loader' }
     ]
   }
